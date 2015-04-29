4f12be438ea14681d5ee12516b716b53da6d1c4b
Dillon Christensen <j.dillon.christensen@gmail.com>
2015-04-29 00:43:05 -0500

    added pages and stuff

---
+++ js/ctrl/SearchCtrl.js
@@ -0,0 +1,63 @@
+(function() {
+    'use strict';
+
+    angular.module('ebay-searcher')
+        .controller('SearchCtrl', function($scope, $http, $window) {
+            $scope.q = '';
+
+            $scope.showSoldListings = function() {
+                return $http.jsonp("http://svcs.ebay.com/services/search/FindingService/v1" +
+                    "?OPERATION-NAME=findCompletedItems" +
+                    "&SERVICE-VERSION=1.0.0" +
+                    "&SECURITY-APPNAME=DillonCh-4ce2-442c-b779-8d0905e2d5e4" +
+                    "&GLOBAL-ID=EBAY-US" +
+                    "&RESPONSE-DATA-FORMAT=JSON" +
+                    "&callback=JSON_CALLBACK" +
+                    "&REST-PAYLOAD" +
+                    "&itemFilter(0).name=SoldItemsOnly" +
+                    "&itemFilter(0).value=true" +
+                    "&paginationInput.entriesPerPage=100" +
+                    "&sortOrder=PricePlusShippingHighest" +
+                    "&keywords=" + encodeURIComponent($scope.q))
+                    .then(function(response) {
+                        var listings = response.data.findCompletedItemsResponse[0].searchResult[0].item
+                            .filter(function(listing) {
+                                return !!listing.primaryCategory[0].categoryName[0].match(/Other|Antiquarian|Fiction|Literature|Book|Children/i)
+                                    && (!!$scope.q.match(/signed|inscribed/) || !listing.title[0].match(/signed|inscribed/i));
+                            });
+                        $scope.soldListings = listings.map(function(listing) {
+                            return {
+                                price: parseFloat(listing.sellingStatus[0].convertedCurrentPrice[0].__value__),
+                                imageUrl: (listing.galleryURL || [])[0],
+                                name: listing.title[0],
+                                date: moment(listing.listingInfo[0].endTime[0]).fromNow(),
+                                url: listing.viewItemURL[0]
+                            };
+                        });
+                        $scope.averageSalePrice = $scope.soldListings.reduce(function(sum, listing) {
+                            return sum + listing.price;
+                        }, 0) / listings.length;
+                        var priceCollection = {};
+                        var mostCommonCount = 0;
+                        var mostCommonPrice = '';
+                        $scope.soldListings.forEach(function(listing) {
+                            var price = '$' + (Math.round(listing.price / 10) * 10).toString();
+                            if(!priceCollection[price]) {
+                                priceCollection[price] = 1;
+                            } else {
+                                priceCollection[price]++;
+                            }
+                            if(mostCommonCount < priceCollection[price]) {
+                                mostCommonCount = priceCollection[price];
+                                mostCommonPrice = price;
+                            }
+                        });
+                        $scope.commonSalePrice = mostCommonPrice;
+                    });
+            };
+
+            $scope.viewListing = function(listing) {
+                $window.open(listing.url, null, '_blank');
+            };
+        });
+}());
