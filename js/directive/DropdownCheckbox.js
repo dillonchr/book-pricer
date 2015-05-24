b6fa5533c92bf674cf8e4c539a8ea3d10d87ed1c
Dillon Christensen <j.dillon.christensen@gmail.com>
2015-05-23 20:13:41 -0500

    updated style to just display pictures and prices

--- js/directive/DropdownCheckbox.js
+++ js/directive/DropdownCheckbox.js
@@ -4,7 +4,7 @@
     angular.module('ebay-searcher')
         .directive('dropdownCheckbox', function() {
             return {
-                template: '<a ng-click="model = !model; $event.stopPropagation();">' +
+                template: '<a ng-click="model = !model">' +
                     '<span class="glyphicon" ng-class="{\'glyphicon-ok\': model, \'glyphicon-remove\': !model}"></span>' +
                     ' {{ ::label }}' +
                 '</a>',
